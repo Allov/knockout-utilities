109734d49829ed4198372bd85208422cac346a02
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:21:36 -0400

    update to koco2

--- src/knockout-utilities.js
+++ src/knockout-utilities.js
@@ -1,51 +1,50 @@
 // Copyright (c) CBC/Radio-Canada. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-define(['knockout', 'jquery'],
-    function(ko, $) {
-        'use strict';
+import ko from 'knockout';
+import $ from 'jquery';
 
-        function KocoUtilities() {}
 
-        //todo: remove when this https://github.com/knockout/knockout/issues/1475
-        KocoUtilities.prototype.koBindingDone = function(element, childElementCount, attempts, includeComments) {
-            return new $.Deferred(function(dfd) {
-                try {
-                    if (!attempts) {
-                        attempts = 400; //default
-                    }
+function KocoUtilities() {}
 
-                    koBindingDoneTest(1, element, dfd, childElementCount, attempts, includeComments);
-                } catch (err) {
-                    dfd.reject(err);
-                }
-            }).promise();
-        };
-
-        //todo: remove when this https://github.com/knockout/knockout/issues/1475
-        function koBindingDoneTest(attempt, element, dfd, childElementCount, attempts, includeComments) {
-            if (attempt >= attempts) {
-                dfd.reject('koBindingDone timeout after ' + attempts + ' attempts.');
-                return;
+//todo: remove when this https://github.com/knockout/knockout/issues/1475
+KocoUtilities.prototype.koBindingDone = function(element, childElementCount, attempts, includeComments) {
+    return new $.Deferred(function(dfd) {
+        try {
+            if (!attempts) {
+                attempts = 400; //default
             }
 
-            // console.info('attempt', attempt, element.childElementCount);
-
-            var bindingDone = includeComments ? element.get(0).childNodes.length > 0 : element.childElementCount > 0;
-
-            if (childElementCount) {
-                bindingDone = includeComments ? element.get(0).childNodes.length === childElementCount : element.childElementCount === childElementCount;
-            }
-
-            if (bindingDone) {
-                dfd.resolve(element);
-                return;
-            }
-
-            setTimeout(function() {
-                koBindingDoneTest(attempt + 1, element, dfd, childElementCount, attempts, includeComments);
-            }, 1);
+            koBindingDoneTest(1, element, dfd, childElementCount, attempts, includeComments);
+        } catch (err) {
+            dfd.reject(err);
         }
+    }).promise();
+};
 
-        return new KocoUtilities();
-    });
+//todo: remove when this https://github.com/knockout/knockout/issues/1475
+function koBindingDoneTest(attempt, element, dfd, childElementCount, attempts, includeComments) {
+    if (attempt >= attempts) {
+        dfd.reject('koBindingDone timeout after ' + attempts + ' attempts.');
+        return;
+    }
+
+    // console.info('attempt', attempt, element.childElementCount);
+
+    var bindingDone = includeComments ? element.get(0).childNodes.length > 0 : element.childElementCount > 0;
+
+    if (childElementCount) {
+        bindingDone = includeComments ? element.get(0).childNodes.length === childElementCount : element.childElementCount === childElementCount;
+    }
+
+    if (bindingDone) {
+        dfd.resolve(element);
+        return;
+    }
+
+    setTimeout(function() {
+        koBindingDoneTest(attempt + 1, element, dfd, childElementCount, attempts, includeComments);
+    }, 1);
+}
+
+export default new KocoUtilities();
